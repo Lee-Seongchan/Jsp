527347f41b0abdd226a790ca8e6351f3a19fe375
lseong96 <lseong96@gmail.com>
2023-09-08 18:12:22 +0900

    확인

--- Farmstory2/src/main/java/kr/co/farmstory2/controller/introduction/Example.java
+++ Farmstory2/src/main/java/kr/co/farmstory2/controller/introduction/Example.java
@@ -2,4 +2,37 @@ package kr.co.farmstory2.controller.introduction;
 
 public class Example {
 	//실험용 커밋입니다.
+		
+
+	public static void main(String[] args) {
+	
+		String nick = "치킨";
+		String nick1 = "피자";
+		String nick2 = "햄버거";
+		String nick3 = "치킨";
+		
+		
+		System.out.println("나의 별명은 " +nick+" 입니다.");
+			
+			if(nick.equals(nick3)) {
+				  if(nick.equals(nick3)){
+				    	System.out.println("피자나라");
+				    	return;
+				    }
+				  System.out.println("국수나라1111");
+			}
+		  
+		    
+		    if(nick.equals(nick3)){
+		    	System.out.println("호빵나라");
+		    }
+		    
+		    System.out.println(1);
+		    System.out.println(2);
+		    System.out.println("치킨공주");
+		    
+		    
+	}
 }
+
+
